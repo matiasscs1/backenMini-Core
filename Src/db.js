a0e2ea960197adcedbd53c457c10d8e60dc4f77b
matiasscs1 <matias.cedeno@udla.edu.ec>
2025-07-01 18:05:04 -0500

    .env

--- Src/db.js
+++ Src/db.js
@@ -1,13 +1,13 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
 async function connectDB() {
-  console.log('Connecting to MongoDB');
+  console.log("Connecting to MongoDB");
   try {
     await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
-    console.log('MongoDB Connected');
+    console.log("MongoDB Connected");
   } catch (err) {
     console.log(err);
   }
--- Src/index.js
+++ Src/index.js
@@ -1,9 +1,15 @@
-import app from './app.js';
-import { connectDB } from './db.js';
+// src/index.js
+import dotenv from "dotenv";
+dotenv.config(); // Esta línea carga las variables de entorno desde .env
 
+import express from "express";
+import { connectDB } from "./db.js";
+
+const app = express();
 const PORT = process.env.PORT || 4000;
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
   connectDB();
 });
+
