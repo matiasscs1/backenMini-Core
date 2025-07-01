f29c3367fe3ac987d0783142701760f87a9fdf06
matiasscs1 <matias.cedeno@udla.edu.ec>
2025-07-01 18:00:08 -0500

    env

--- Src/db.js
+++ Src/db.js
@@ -3,7 +3,7 @@ import mongoose from 'mongoose';
 async function connectDB() {
   console.log('Connecting to MongoDB');
   try {
-    await mongoose.connect(process.env.MONGO_URI || 'mongodb+srv://matiaz490:1234@terranovaconect.m881eys.mongodb.net/MiniCore?retryWrites=true&w=majority&appName=terranovaConect', {
+    await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
